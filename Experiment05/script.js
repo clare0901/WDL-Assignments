c7851ff9b0cbc85634c04b50474154d5d9485962
clare0901 <clarerebello0901@gmail.com>
2020-10-27 19:48:53 +0530

    Add

---
+++ Experiment05/script.js
@@ -0,0 +1,7 @@
+function Validation(){
+    id = document.getElementById('user_id');
+    password = document.getElementById('pass');
+    name = document.getElementById('name');
+    address = document.getElementById('addr');
+    country = document.getElementById('');
+}
